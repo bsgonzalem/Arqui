a6d3cd14fab9f57bc52f3a0f905c99e11fff422d
bsgonzalem <bsgonzalem@eafit.edu.co>
2026-08-22 16:34:12 -0500

    Tutorial 3

--- laravelcourse/app/Http/Controllers/ProductController.php
+++ laravelcourse/app/Http/Controllers/ProductController.php
@@ -28,7 +28,7 @@ class ProductController extends Controller
 
     public function create(): View
     {
-        $viewData = []; // to be sent to the view
+        $viewData = [];
 
         $viewData['title'] = 'Create product';
 
--- laravelcourse/routes/web.php
+++ laravelcourse/routes/web.php
@@ -24,19 +24,22 @@ Route::get('/about', function () {
 
 Route::get('/contact', 'App\Http\Controllers\HomeController@contact')->name('home.contact');
 
-Route::get('/products', 'App\Http\Controllers\ProductController@index')
-    ->name('product.index');
+Route::get('/products', 'App\Http\Controllers\ProductController@index')->name('product.index');
 
-Route::get('/products/create', 'App\Http\Controllers\ProductController@create')
-    ->name('product.create');
+Route::get('/products/create', 'App\Http\Controllers\ProductController@create')->name('product.create');
 
-Route::post('/products/save', 'App\Http\Controllers\ProductController@save')
-    ->name('product.save');
+Route::post('/products/save', 'App\Http\Controllers\ProductController@save')->name('product.save');
 
-Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')
-    ->name('product.show');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->name('product.show');
 
-Route::get('/products/success', 'App\Http\Controllers\ProductController@success')
-    ->name('product.success');
+Route::get('/products/success', 'App\Http\Controllers\ProductController@success')->name('product.success');
+
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name('image.index');
+
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name('image.save');
+
+Route::get('/image-not-di', 'App\Http\Controllers\ImageNotDIController@index')->name('imagenotdi.index');
+
+Route::post('/image-not-di/save', 'App\Http\Controllers\ImageNotDIController@save')->name('imagenotdi.save');
 
 Auth::routes();
